b68efb96c909f3f401c37b28b515dba197f84b8f
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-24 17:40:16 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets, :dog, :cat
+
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+
+  def initialize(species)
+    @species = species
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def pets
+    @@pets
+  end
+
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+      dog.mood = 'happy'
+    end
+  end
+
+  def play_with_cats
+    @@pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
+  def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
+  end
+
 end
